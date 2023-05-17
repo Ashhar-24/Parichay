9d175a35086e67dca5955ff5af871da6b50f50e6
Md Ashhar <mdashhar01@gmail.com>
2023-05-17 21:31:37 +0530

    links added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,21 +9,15 @@ const Portfolio = () => {
         {
             id:1,
             src: techBuddy,
-            //href: demo link,
-            href: 'https://techbuddy-ashhar01.000webhostapp.com/',
-            href_git: 'https://github.com/Ashhar-24/techBuddy',
-            // same as done in social links derefrencing
+            demoHref: 'https://techbuddy-ashhar01.000webhostapp.com/',
+            codeHref: 'https://github.com/Ashhar-24/techBuddy',
         },
         {
             id:2,
             src: portfolio,
-            href_demo: '',
-            href_git: ' ',
+            demoHref: 'https://github.com/Ashhar-24/Parichay',
+            codeHref: 'https://github.com/Ashhar-24/Parichay',
         },
-        // {
-        //     id:3,
-        //     src: navbar
-        // },
     ]
 
 
@@ -40,17 +34,17 @@ const Portfolio = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 '>
 
             {
-                portfolios.map(({id, src, href, href_git}) => (
+                portfolios.map(({id, src, demoHref, codeHref}) => (
                     
                 <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                     <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
 
                     <div className='flex items-center justify-center'>
-                        <a href={href} target='_blank'  rel="noreferrer" >
+                        <a href={demoHref} target='_blank'  rel="noreferrer" >
                         <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' target='_blank' rel="noreferrer">Demo</button>
                         </a>
                         
-                        <a href={href} target='_blank'  rel="noreferrer" >
+                        <a href={codeHref} target='_blank'  rel="noreferrer" >
                         <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >Code</button>
                         </a>
                     </div>
